c2439c9956ad9b7b0d7234a18894ff2e6de54c9b
Pedro vieira <pedro97vieira@gmail.com>
2021-02-26 12:50:00 +0000

    criação da estrura da pg inicial

--- app/src/main/java/com/example/cityreports/MainActivity.kt
+++ app/src/main/java/com/example/cityreports/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.cityreports
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    fun buttonLogin(view: View){
+
+        //Colocar validação do login aqui
+        val intent = Intent(this,PaginaInicial::class.java)
+        startActivity(intent)
     }
 }
