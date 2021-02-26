8476fb5e6977cd31b2d14d18ba7f1ea06453c0f4
Pedro vieira <pedro97vieira@gmail.com>
2021-02-26 17:46:18 +0000

    Estrutra do menu

--- app/src/main/java/com/example/cityreports/MenuActivity.kt
+++ app/src/main/java/com/example/cityreports/MenuActivity.kt
@@ -1,21 +1,19 @@
 package com.example.cityreports
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import com.google.android.material.bottomappbar.BottomAppBar
 
-class PaginaInicial : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pagina_inicial)
-        val appbar = findViewById<BottomAppBar>(R.id.bottomAppBar)
-        appbar.setNavigationOnClickListener{
+        setContentView(R.layout.activity_menu)
+        val cardHome = findViewById<CardView>(R.id.cardHome)
+        cardHome.setOnClickListener{
             Toast.makeText(applicationContext, "aadsat", Toast.LENGTH_LONG).show()
-            val intent = Intent(this,MenuActivity::class.java)
-            startActivity(intent)
         }
-    }
 
+    }
 }
