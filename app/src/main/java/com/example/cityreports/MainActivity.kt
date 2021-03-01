8d43b5c072a084ac31f1dc049f8ae512464c948a
Pedro vieira <pedro97vieira@gmail.com>
2021-03-01 11:25:26 +0000

    estrutura da pagina inical done com mapa

--- app/src/main/java/com/example/cityreports/MainActivity.kt
+++ app/src/main/java/com/example/cityreports/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     fun buttonLogin(view: View){
 
         //Colocar validação do login aqui
-        val intent = Intent(this,PaginaInicial::class.java)
+        val intent = Intent(this,Initial_page::class.java)
         startActivity(intent)
     }
 }
