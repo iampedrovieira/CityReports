9838aa62f105d261b8143fd03b372551b55c7008
Pedro Vieira <61158116+iampedrovieira@users.noreply.github.com>
2021-03-04 14:53:48 +0000

    Fe initial page structure done (#4)
    * estrutura da pagina inical done com mapa
    
    * estrutura do menu done
    
    * fix login layout

--- app/src/main/java/com/example/cityreports/MenuActivity.kt
+++ app/src/main/java/com/example/cityreports/MenuActivity.kt
@@ -1,7 +1,9 @@
 package com.example.cityreports
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.google.android.material.bottomappbar.BottomAppBar
@@ -10,10 +12,17 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        val cardHome = findViewById<CardView>(R.id.cardHome)
-        cardHome.setOnClickListener{
-            Toast.makeText(applicationContext, "aadsat", Toast.LENGTH_LONG).show()
+        val closeMenu = findViewById<ImageView>(R.id.imageBack)
+        closeMenu.setOnClickListener{
+            finish()
         }
+        val logout = findViewById<CardView>(R.id.cardLogout)
+        logout.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
 
     }
 }
