79d219d30ebde6d6fd23aa21ff0d625147d8b134
Pedro Vieira <61158116+iampedrovieira@users.noreply.github.com>
2021-03-13 16:56:13 +0000

    sp login done (#9)

--- app/src/main/java/com/example/cityreports/MainActivity.kt
+++ app/src/main/java/com/example/cityreports/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.cityreports
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,13 +11,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedPref:SharedPreferences = getSharedPreferences(getString(R.string.sp_login),Context.MODE_PRIVATE)
+        val loginValue = sharedPref.getBoolean(getString(R.string.sp_login_value),false)
+        if(loginValue){
+            val intent = Intent(this,Initial_page::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 
     fun buttonLogin(view: View){
 
         //Colocar validação do login aqui
+
         val intent = Intent(this,Initial_page::class.java)
         //Clear stack
+        val sharedPref:SharedPreferences = getSharedPreferences(getString(R.string.sp_login),Context.MODE_PRIVATE)
+        with(sharedPref.edit()){
+            putBoolean(getString(R.string.sp_login_value),true)
+            commit()
+        }
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
 
