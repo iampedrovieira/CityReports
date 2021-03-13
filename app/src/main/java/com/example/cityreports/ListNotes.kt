99f94269aee06d8a18329f279d3422278f3d1ce5
Pedro Vieira <61158116+iampedrovieira@users.noreply.github.com>
2021-03-13 16:39:43 +0000

    Be notes (#8)
    * Delete back done
    
    * DELETE DONE
    
    * notes done
    
    * note bug fix

--- app/src/main/java/com/example/cityreports/ListNotes.kt
+++ app/src/main/java/com/example/cityreports/ListNotes.kt
@@ -1,21 +1,34 @@
 package com.example.cityreports
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.size
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cityreports.adapters.NoteAdapter
 import com.example.cityreports.adapters.OnItemClickListener
-import com.example.cityreports.dataclasses.Note
+import com.example.cityreports.db.NoteDB
+import com.example.cityreports.db.NoteRepository
+import com.example.cityreports.entities.Note
+import com.example.cityreports.viewModel.NoteViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ListNotes : AppCompatActivity(),OnItemClickListener {
 
-    private lateinit var list:ArrayList<Note>
-
+    private lateinit var noteViewModel:NoteViewModel
+    private val newWordActivityRequestCode = 1
+    private lateinit var adapter:NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_notes)
@@ -24,14 +37,47 @@ class ListNotes : AppCompatActivity(),OnItemClickListener {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.notes)
 
-        list = ArrayList<Note>()
-        for(i in 0 until 30){
-            list.add(Note(i,"E que tudo fixe?"))
-        }
-
         val recycler = findViewById<RecyclerView>(R.id.recycler_Notes)
-        recycler.adapter = NoteAdapter(list,this)
+         adapter = NoteAdapter(this,this)
+        recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(this)
+
+        //View model
+        noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+        noteViewModel.allNotes.observe(this, Observer { notes ->
+            notes?.let { adapter.setNotes(it) }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK){
+            if(data?.getBooleanExtra("New",false) == true){
+                val desc = data.getStringExtra("Description")
+                if(desc != null){
+                    val note = Note(description = desc)
+                    noteViewModel.insert(note)
+                }
+
+            }else{
+                if(data?.getIntExtra("ID",-1) != null) {
+                    val nid = data.getIntExtra("ID", -1)
+                    val desc = data.getStringExtra("Description")
+                    if(desc != null){
+                        val note = Note(nid,desc)
+                        noteViewModel.updateNote(note)
+                    }
+                }
+            }
+
+        }
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_CANCELED){
+
+            if(data?.getIntExtra("ID",-1) != null){
+                    val nid = data.getIntExtra("ID",-1)
+                    noteViewModel.deleteNote(nid)
+            }
+        }
 
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -41,19 +87,16 @@ class ListNotes : AppCompatActivity(),OnItemClickListener {
         return super.onOptionsItemSelected(item)
     }
     //This method is the interface method
-    override fun onItemClick(position: Int) {
-        val intent = Intent(this,NoteOpen::class.java)
-        startActivity(intent)
-
-        /*Vai abrir a NoteOpen com os dados da nota
-            Ou envio os dados como parametro
-            ou envio o id e vai buscar os dados a db local
-
-        * */
+    override fun onItemClick(note: Note) {
+        val intent = Intent(this@ListNotes, NoteOpen::class.java)
+        intent.putExtra("ID",note.id)
+        intent.putExtra("Description",note.description)
+        startActivityForResult(intent, newWordActivityRequestCode)
     }
 
-    fun buttonCreateNote(view: View){
-        val intent = Intent(this,NoteOpen::class.java)
-        startActivity(intent)
+     fun buttonCreateNote(view: View){
+             val intent = Intent(this@ListNotes, NoteOpen::class.java)
+             intent.putExtra("New",true)
+             startActivityForResult(intent, newWordActivityRequestCode)
     }
 }
--- app/src/main/java/com/example/cityreports/NoteOpen.kt
+++ app/src/main/java/com/example/cityreports/NoteOpen.kt
@@ -1,21 +1,34 @@
 package com.example.cityreports
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class NoteOpen : AppCompatActivity() {
+
+    private lateinit var descText:TextInputEditText
+    private var noteId:Int? = null
+    private var newNote:Boolean =false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_open)
         setSupportActionBar(findViewById(R.id.toolbarNote))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.note)
-
+        noteId = intent.getIntExtra("ID",0)
+        descText = findViewById(R.id.textAreaNote2)
+        descText.setText(intent.getStringExtra("Description"))
+        newNote=intent.getBooleanExtra("New",false)
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
@@ -24,15 +37,35 @@ class NoteOpen : AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == android.R.id.home){
-            onBackPressed()
+            val replyIntent = Intent()
+            if(descText.text.toString()==""){
+                replyIntent.putExtra("ID",noteId)
+                setResult(Activity.RESULT_CANCELED,replyIntent)
+                Toast.makeText(applicationContext, "Note Deleted", Toast.LENGTH_LONG).show()
+            }
+            finish()
         }
         if(item.itemId == R.id.Delete){
-            Toast.makeText(applicationContext, "delete", Toast.LENGTH_LONG).show()
+            val replyIntent = Intent()
+            replyIntent.putExtra("ID",noteId)
+            setResult(Activity.RESULT_CANCELED,replyIntent)
+            Toast.makeText(applicationContext, "Note Deleted", Toast.LENGTH_LONG).show()
+            finish()
         }
         return super.onOptionsItemSelected(item)
     }
 
     fun saveNote(view: View){
         Toast.makeText(applicationContext, "Save", Toast.LENGTH_LONG).show()
+        val replyIntent = Intent()
+        replyIntent.putExtra("ID",noteId)
+        replyIntent.putExtra("Description",descText.text.toString())
+        if(newNote){
+            replyIntent.putExtra("New",true)
+            replyIntent.putExtra("Description",descText.text.toString())
+        }
+        setResult(Activity.RESULT_OK,replyIntent)
+        finish()
+
     }
 }
